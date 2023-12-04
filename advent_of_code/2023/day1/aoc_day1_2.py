00eb71db94e95f30ba678683cefae0660c778c43
Abid Khan <abidkhan484@gmail.com>
2023-12-04 12:38:06 +0600

    remove unnecessary line

--- advent_of_code/2023/day1/aoc_day1_2.py
+++ advent_of_code/2023/day1/aoc_day1_2.py
@@ -12,8 +12,7 @@ input_array = [line.strip('\n') for line in fileinput.input(files=[path])]
 
 number_key_value = {'nine': '9', 'eight': '8', 'seven': '7', 'six': '6',
   'five': '5', 'four': '4', 'three': '3', 'two': '2', 'one': '1'}
-pattern = '|'.join(number_key_value.keys()) + '|' + '\d'
-pattern = '(?=('+'|'.join(number_key_value.keys())+'|\\d))'
+pattern = '(?=('+'|'.join(number_key_value.keys())+'|\d))'
 
 output = 0
 for line in input_array:
